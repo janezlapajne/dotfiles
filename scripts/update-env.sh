123d99f9da1e498e2d982dc0860c3f112008154b
janezlapajne <lapajne.janez@gmail.com>
2024-03-17 23:56:23 +0100

    Update .env.example and update-env.sh scripts

--- scripts/update-env.sh
+++ scripts/update-env.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 #
 # Update .env.example file from .env file
 
@@ -12,17 +12,18 @@ echo "################################################
 # Example ".env" file
 # Use command:  $ tail -n +7 .env.example > .env
 # -> to create .env file from .env.example
-################################################ \n" >$output_file
+################################################ " >$output_file
+echo "" >>$output_file
 
 # Create the example file
 while IFS='=' read -r key value; do
-	# if key and value not empty:
-	if [ -n "$key" ] && [ -n "$value" ]; then
-		echo "$key=" >>$output_file
 	# if comment
-	elif case $key in \#*) true ;; *) false ;; esac then
-		echo "$key" >>$output_file
+	if [[ $key == \#* ]]; then
+		echo "$key" >>"$output_file"
+	# if key is not empty:
+	elif [[ -n $key ]]; then
+		echo "$key=" >>"$output_file"
 	else
-		echo "" >>$output_file
+		echo "" >>"$output_file"
 	fi
 done <"$input_file"
