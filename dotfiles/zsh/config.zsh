27e4fcf752ab9175b55afa28378c795673d11ae3
janezlapajne <lapajne.janez@gmail.com>
2024-04-10 10:21:32 +0200

    Update zsh config and plugins -- removed unnecessary vim configs

--- dotfiles/zsh/config.zsh
+++ dotfiles/zsh/config.zsh
@@ -1,13 +1,11 @@
-# OH-MY-ZSH configuration
+# Oh-my-zsh configuration
 ZSH_THEME="robbyrussell"
 # ZSH_DISABLE_COMPFIX="true"
 ENABLE_CORRECTION="true"
 COMPLETION_WAITING_DOTS="true"
 HIST_STAMPS="dd.mm.yyyy"
 
-# Plugins
-# Download zip (do not clone) and extract to .oh-my-zsh\custom\plugins + rename
-# Must have unix line ending
+# Oh-my-zsh Plugins
 plugins=(
 	aliases
 	alias-finder
@@ -23,7 +21,16 @@ plugins=(
 	zsh-interactive-cd
 )
 
-source $ZSH/oh-my-zsh.sh
+# vi-mode
+VI_MODE_SET_CURSOR=true
+
+# alias-finder
+zstyle ':omz:plugins:alias-finder' autoload yes # disabled by default
+zstyle ':omz:plugins:alias-finder' longer yes   # disabled by default
+zstyle ':omz:plugins:alias-finder' exact yes    # disabled by default
+zstyle ':omz:plugins:alias-finder' cheaper yes  # disabled by default
+
+source $ZSH/oh-my-zsh.sh # ----> init oh-my-zsh
 
 # History
 HISTFILE=~/.zsh_history
@@ -49,49 +56,11 @@ setopt IGNORE_EOF
 setopt NO_LIST_BEEP
 unsetopt BEEP # Turn off all beeps
 
-# Use vim keys in tab complete menu:
-bindkey -M menuselect 'h' vi-backward-char
-bindkey -M menuselect 'k' vi-up-line-or-history
-bindkey -M menuselect 'l' vi-forward-char
-bindkey -M menuselect 'j' vi-down-line-or-history
-bindkey -v '^?' backward-delete-char
-
 # Add functions to fpath
 chmod go-w "$DOTFILES_ZSH"
 chmod go-w "$DOTFILES_ZSH/functions"
 fpath=($DOTFILES_ZSH/functions $fpath)
 autoload -U $DOTFILES_ZSH/functions/*(:t)
-
-# Change cursor shape for different vi modes.
-function zle-keymap-select() {
-	case $KEYMAP in
-	vicmd) echo -ne '\e[1 q' ;;        # block
-	viins | main) echo -ne '\e[5 q' ;; # beam
-	esac
-}
-zle -N zle-keymap-select
-zle-line-init() {
-	zle -K viins # initiate `vi insert` as keymap (can be removed if `bindkey -V` has been set elsewhere)
-	echo -ne "\e[5 q"
-}
-zle -N zle-line-init
-echo -ne '\e[5 q'                # Use beam shape cursor on startup.
-preexec() { echo -ne '\e[5 q'; } # Use beam shape cursor for each new prompt.
-
-# vi mode
-bindkey -v
-export KEYTIMEOUT=1
-
-# Edit line in code with ctrl-e:
-autoload -z edit-command-line
-zle -N edit-command-line
-bindkey '^e' edit-command-line
-
-# alias-finder plugin
-zstyle ':omz:plugins:alias-finder' autoload yes # disabled by default
-zstyle ':omz:plugins:alias-finder' longer yes   # disabled by default
-zstyle ':omz:plugins:alias-finder' exact yes    # disabled by default
-zstyle ':omz:plugins:alias-finder' cheaper yes  # disabled by default
 
 # Init Atuin for better history
 eval "$(atuin init zsh)"
