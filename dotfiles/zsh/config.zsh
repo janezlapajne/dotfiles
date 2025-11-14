f64bca5d019e5b330f36c06d603c645d55c72f84
janezlapajne <lapajne.janez@gmail.com>
2025-11-14 23:35:11 +0100

    fix: Disable correction options in zsh configuration for improved command handling

--- dotfiles/zsh/config.zsh
+++ dotfiles/zsh/config.zsh
@@ -50,7 +50,9 @@ setopt HIST_REDUCE_BLANKS
 setopt LOCAL_OPTIONS # allow functions to have local options
 setopt LOCAL_TRAPS   # allow functions to have local traps
 setopt PROMPT_SUBST
-setopt CORRECT
+# setopt CORRECT
+unsetopt correctall
+unsetopt correct
 setopt COMPLETE_IN_WORD
 setopt IGNORE_EOF
 setopt NO_LIST_BEEP
