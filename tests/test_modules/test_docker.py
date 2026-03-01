b090fdde7bde699ffac806d55b9b4c1837131c54
janezlapajne <lapajne.janez@gmail.com>
2026-03-02 00:03:06 +0100

    #333 Remove obsolete test files and configurations to streamline testing framework

--- tests/test_modules/test_docker.py
+++
@@ -1,8 +0,0 @@
-from cli.config import Config
-from modules.docker import DockerModule
-
-
-class TestDockerModule:
-    def test_has_name(self, macos_config: Config):
-        module = DockerModule(macos_config)
-        assert module.name == "docker"
