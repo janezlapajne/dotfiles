62025d45c2336b72b66e0635e666aec161dc3193
janezlapajne <lapajne.janez@gmail.com>
2026-03-28 20:20:44 +0100

    Remove Python aliases from aliases.zsh and move Worktrunk aliases to a new file

--- conf/dotfiles/system/aliases.zsh
+++ conf/dotfiles/system/aliases.zsh
@@ -6,8 +6,6 @@ alias grep='grep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
 
-alias py='python3'
-
 # Easy cd
 alias .1='cd ../'
 alias .2='cd ../../'
@@ -47,14 +45,6 @@ alias rp='realpath'
 
 # Copy to clipboard
 alias clip='pbcopy'
-
-# Worktrunk
-alias wts='wt switch'
-alias wtc='wt switch --create'
-alias wtl='wt list --full'
-alias wtr='wt remove'
-alias wtlogs='wt config state logs'
-alias wtlog='cat "$(find "$(realpath "$(git rev-parse --git-common-dir)")/wt-logs" -name "*.log" -size +0 -print0 | xargs -0 ls -t | head -1)"'
 
 # Process management
 alias fk='fkill'
